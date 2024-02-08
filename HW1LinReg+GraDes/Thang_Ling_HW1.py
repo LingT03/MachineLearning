323557674a96eb3f1df7c5f66350af4af43f55ab
Ling._.T <Lingthang03@gmail.com>
2024-02-08 12:35:31 -0700

    Feb 8

--- HW1LinReg+GraDes/Thang_Ling_HW1.py
+++ HW1LinReg+GraDes/Thang_Ling_HW1.py
@@ -8,9 +8,10 @@ x_data = np.array([35., 38., 31., 20., 22., 25., 17., 60., 8., 60.])
 Y_data= np.array([129.54611622, 135.54611622, 121.54611622, 99.54611622,
 103.54611622, 109.54611622, 93.54611622, 179.54611622, 75.54611622, 179.54611622])
 
+print(x_data)
 
 
-# 
+
 
 def gradient_descent(x_data, Y_data, b:float, iterations: int, leazrn_rate: float, b_history: list, w_history: list):
     for i in range(iterations):
